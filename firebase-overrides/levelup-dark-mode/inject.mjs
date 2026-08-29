1f3b3c9cf6e4a14e84280df00cd89d73e5dbeed9
hrt14 <151790680+hrt14@users.noreply.github.com>
2026-08-29 20:34:01 +0900

    Upgrade LEVEL UP social share graphic
    * Add LEVEL UP share graphic chunk 0
    
    * Add LEVEL UP share graphic chunk 1
    
    * Add LEVEL UP share graphic chunk 2
    
    * Add LEVEL UP share graphic chunk 3
    
    * Add LEVEL UP share graphic chunk 4
    
    * Add LEVEL UP share graphic chunk 5
    
    * Add LEVEL UP share graphic chunk 6
    
    * Add LEVEL UP approved share graphic v3 injector
    
    * Wire LEVEL UP share graphic v3 after result sharing
    
    * Trigger LEVEL UP share graphic v3 production deploy

--- firebase-overrides/levelup-dark-mode/inject.mjs
+++ firebase-overrides/levelup-dark-mode/inject.mjs
@@ -5,3 +5,4 @@ await import('../levelup-search-landings-v2/inject.mjs');
 await import('../../scripts/enforce-levelup-mobile-typography.mjs');
 await import('../levelup-acquisition-v2/inject.mjs');
 await import('../levelup-share-results-v2/inject.mjs');
+await import('../levelup-share-graphic-v3/inject.mjs');
