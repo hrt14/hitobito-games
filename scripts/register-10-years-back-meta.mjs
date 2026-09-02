591092aed6fe033f99cf661f81f308616440fdd9
hrt14 <151790680+hrt14@users.noreply.github.com>
2026-09-02 09:01:29 +0900

    Include breakthrough 90 metadata

--- scripts/register-10-years-back-meta.mjs
+++ scripts/register-10-years-back-meta.mjs
@@ -3,6 +3,7 @@ import './register-thick-self-meta.mjs';
 import './register-mienai-zandaka-meta.mjs';
 import './register-work-avalanche-meta.mjs';
 import './register-yesterday-self-meta.mjs';
+import './register-breakthrough-90-meta.mjs';
 
 GAME_META['10-years-back'] = [
   'levelup',
